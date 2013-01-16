de92426ca6c6c48513a06fa94c31ce9fe9a00377
Matthias Bolte <matthias@tinkerforge.com>
2013-01-16 20:17:47 +0100

    Update Java example and use a final local instead of a global static variable

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -1,21 +1,19 @@
 import com.tinkerforge.BrickServo;
 import com.tinkerforge.IPConnection;
+import com.tinkerforge.TimeoutException;
+import com.tinkerforge.NotConnectedException;
 
 public class ExampleCallback {
 	private static final String host = "localhost";
 	private static final int port = 4223;
 	private static final String UID = "a4LCMm3K2bS"; // Change to your UID
 
-	// Declare servo static, so the listener can use it. In a real program you probably
-	// want to make a real listener class (not the anonym inner class) and pass the servo
-	// reference to it. But we want to keep the examples as short es possible.
-	static BrickServo servo;
-
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
-		servo = new BrickServo(UID, ipcon); // Create device object
+		// Note: Declare servo final, so the listener can access it
+		final BrickServo servo = new BrickServo(UID, ipcon); // Create device object
 
 		ipcon.connect(host, port); // Connect to brickd
 		// Don't use device before ipcon is connected
@@ -27,16 +25,16 @@ public class ExampleCallback {
 				if(position == 9000) {
 					System.out.println("Position: 90°, going to -90°");
 					try {
-						ExampleCallback.servo.setPosition(servoNum, (short)-9000);
-					} catch(IPConnection.TimeoutException e) {
-					} catch(IPConnection.NotConnectedException e) {
+						servo.setPosition(servoNum, (short)-9000);
+					} catch(TimeoutException e) {
+					} catch(NotConnectedException e) {
 					}
 				} else if(position == -9000) {
 					System.out.println("Position: -90°, going to 90°");
 					try {
-						ExampleCallback.servo.setPosition(servoNum, (short)9000);
-					} catch(IPConnection.TimeoutException e) {
-					} catch(IPConnection.NotConnectedException e) {
+						servo.setPosition(servoNum, (short)9000);
+					} catch(TimeoutException e) {
+					} catch(NotConnectedException e) {
 					}
 				} else {
 					// Can only happen if another program sets velocity
