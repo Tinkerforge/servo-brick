a187e22b9a255f2450aeb32178dadcc534c6ba97
Matthias Bolte <matthias@tinkerforge.com>
2013-02-18 16:20:53 +0100

    Add Visual Basic .NET examples

---
+++ software/examples/vbnet/ExampleCallback.vb
@@ -0,0 +1,45 @@
+Imports Tinkerforge
+
+Module ExampleCallback
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "6wywsn" ' Change to your UID
+
+    ' Callback function for distance callback (parameter has unit mm)
+    Sub ReachedCB(ByVal sender As BrickServo, ByVal servoNum As Byte, ByVal position As Short)
+        If position = 9000 Then
+            System.Console.WriteLine("Position: 90°, going to -90°")
+            sender.SetPosition(servoNum, -9000)
+        Else If position = -9000 Then
+            System.Console.WriteLine("Position: -90°, going to 90°")
+            sender.SetPosition(servoNum, 9000)
+        Else
+            ' Can only happen if another program sets position
+            System.Console.WriteLine("Error")
+        End If
+    End Sub
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim servo As New BrickServo(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Register "position reached callback" to ReachedCB
+        ' ReachedCB will be called every time a position set with
+        ' SetPosition is reached
+        AddHandler servo.PositionReached, AddressOf ReachedCB
+
+        ' Set velocity to 100°/s. This has to be smaller or equal to
+        ' maximum velocity of the servo, otherwise ReachedCB will be
+        ' called too early
+        servo.SetVelocity(0, 10000)
+        servo.SetPosition(0, 9000)
+        servo.Enable(0)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
---
+++ software/examples/vbnet/ExampleConfiguration.vb
@@ -0,0 +1,45 @@
+Imports Tinkerforge
+
+Module ExampleConfiguration
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "6wywsn" ' Change to your UID
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim servo As New BrickServo(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Configure two servos with voltage 5.5V
+        ' Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
+        '          and operating angle -100 to 100°
+        '
+        ' Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95
+        '          to 1.95ms and operating angle -90 to 90°
+        servo.SetOutputVoltage(5500)
+
+        servo.SetDegree(0, -10000, 10000)
+        servo.SetPulseWidth(0, 1000, 2000)
+        servo.SetPeriod(0, 19500)
+        servo.SetAcceleration(0, 1000) ' Slow acceleration
+        servo.SetVelocity(0, &HFFFF) ' Full speed
+
+        servo.SetDegree(5, -9000, 9000)
+        servo.SetPulseWidth(5, 950, 1950)
+        servo.SetPeriod(5, 20000)
+        servo.SetAcceleration(5, &HFFFF) ' Full acceleration
+        servo.SetVelocity(5, &HFFFF) ' Full speed
+
+        servo.SetPosition(0, 10000) ' Set to most right position
+        servo.Enable(0)
+
+        servo.SetPosition(5, -9000) ' Set to most left position
+        servo.Enable(5)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
