d80aa80f772c79be13dba8192f2d46ef56baa875
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 16:03:21 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickServo;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '5VF5vG'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $servo = BrickServo->new(&UID, $ipcon); # Create device object
+
+# Use position reached callback to swing back and forth
+sub cb_reached
+{
+    my ($servo_num, $position) = @_;
+
+    if ($position == 9000)
+    {
+        print "\nPosition: 90°, going to -90°\n";
+        $servo->set_position($servo_num, -9000);
+    }
+    elsif ($position == -9000)
+    {
+        print "\nPosition: -90°, going to 90°\n";
+        $servo->set_position($servo_num, 9000);
+    }
+    else
+    {
+        print "\nError\n"; # Can only happen if another program sets position
+    }
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Register "position reached callback" to cb_reached
+# cb_reached will be called every time a position set with
+# set_position is reached
+$servo->register_callback($servo->CALLBACK_POSITION_REACHED, 'cb_reached');
+$servo->enable_position_reached_callback();
+
+# Set velocity to 100°/s. This has to be smaller or equal to 
+# maximum velocity of the servo, otherwise cb_reached will be
+# called too early
+$servo->set_velocity(0, 10000) ;
+$servo->set_position(0, 9000);
+$servo->enable(0);
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
---
+++ software/examples/perl/example_configuration.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickServo;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '5VF5vG'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $servo = BrickServo->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Configure two servos with voltage 5.5V
+# Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
+#          and operating angle -100 to 100°
+#
+# Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95 
+#          to 1.95ms and operating angle -90 to 90°
+$servo->set_output_voltage(5500);
+
+$servo->set_degree(0, -10000, 10000);
+$servo->set_pulse_width(0, 1000, 2000);
+$servo->set_period(0, 19500);
+$servo->set_acceleration(0, 1000); # Slow acceleration
+$servo->set_velocity(0, 0xFFFF); # Full speed
+
+$servo->set_degree(5, -9000, 9000);
+$servo->set_pulse_width(5, 950, 1950);
+$servo->set_period(5, 20000);
+$servo->set_acceleration(5, 0xFFFF); # Full acceleration
+$servo->set_velocity(5, 0xFFFF); # Full speed
+
+$servo->set_position(0, 10000); # Set to most right position
+$servo->enable(0);
+
+$servo->set_position(5, -9000); # Set to most left position
+$servo->enable(5);
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
