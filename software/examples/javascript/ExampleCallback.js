698d50f7c2e9a8f8e9c17d3baa925a281691b4b9
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 14:18:53 +0100

    javascript: adding examples

---
+++ software/examples/javascript/ExampleCallback.js
@@ -0,0 +1,62 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickServo = require('Tinkerforge/BrickServo');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = '6qCdfo';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var servo = new BrickServo(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
+            console.log('Error: Already connected');        
+        }
+    }
+);//Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        servo.enablePositionReachedCallback();
+        // Set velocity to 100°/s. This has to be smaller or equal to 
+        // maximum velocity of the servo, otherwise cb_reached will be
+        // called too early
+        servo.setVelocity(0, 10000); 
+        servo.setPosition(0, 9000);
+        servo.enable(0);  
+    }
+);
+
+// Register "position reached callback"
+// Callback "position reached callback" will be called every time a position set with
+// set_position is reached
+servo.on(BrickServo.CALLBACK_POSITION_REACHED,
+    function(servoNum, position) {
+        if(position === 9000) {
+            console.log('Position: 90°, going to -90°');
+            servo.setPosition(servoNum, -9000);
+        }
+        else if(position === -9000) {
+            console.log('Position: -90°, going to 90°');
+            servo.setPosition(servoNum, 9000);
+        }
+        else {
+            console.log('Error');// Can only happen if another program sets position
+        }
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data', function(data) {
+	    ipcon.disconnect(
+            function(error) {
+                if(error === IPConnection.ERROR_NOT_CONNECTED) {
+                    console.log('Error: Not connected');        
+                }
+            }
+        );
+process.exit(0);
+});
+
---
+++ software/examples/javascript/ExampleConfiguration.js
@@ -0,0 +1,56 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickServo = require('Tinkerforge/BrickServo');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = '6qCdfo';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var servo = new BrickServo(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
+            console.log('Error: Already connected');        
+        }
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Configure two servos with voltage 5.5V
+        // Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
+        // and operating angle -100 to 100°
+        // Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95 
+        // to 1.95ms and operating angle -90 to 90°
+        servo.setOutputVoltage(5500);
+        servo.setDegree(0, -10000, 10000);
+        servo.setPulseWidth(0, 1000, 2000);
+        servo.setPeriod(0, 19500);
+        servo.setAcceleration(0, 1000);//Slow acceleration
+        servo.setVelocity(0, 0xFFFF);//Full speed
+        servo.setDegree(5, -9000, 9000);
+        servo.setPulseWidth(5, 950, 1950);
+        servo.setPeriod(5, 20000);
+        servo.setAcceleration(5, 0xFFFF);//Full acceleration
+        servo.setVelocity(5, 0xFFFF);//Full speed
+        servo.setPosition(0, 10000);//Set to most right position
+        servo.enable(0);
+        servo.setPosition(5, -9000);//Set to most left position
+        servo.enable(5);      
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data', function(data) {
+	    ipcon.disconnect(
+            function(error) {
+                if(error === IPConnection.ERROR_NOT_CONNECTED) {
+                    console.log('Error: Not connected');        
+                }
+            }
+        );
+process.exit(0);
+});
+
