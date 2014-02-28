c45d73af4b975df479f0b9bb3afcf6dd50dae899
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:24:17 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -2,27 +2,27 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '6qCdfo';// Change to your UID
+var UID = '6qCdfo'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var servo = new Tinkerforge.BrickServo(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var servo = new Tinkerforge.BrickServo(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         servo.enablePositionReachedCallback();
-        // Set velocity to 100°/s. This has to be smaller or equal to 
+        // Set velocity to 100°/s. This has to be smaller or equal to
         // maximum velocity of the servo, otherwise cb_reached will be
         // called too early
-        servo.setVelocity(0, 10000); 
+        servo.setVelocity(0, 10000);
         servo.setPosition(0, 9000);
-        servo.enable(0);  
+        servo.enable(0);
     }
 );
 
@@ -40,7 +40,7 @@ servo.on(Tinkerforge.BrickServo.CALLBACK_POSITION_REACHED,
             servo.setPosition(servoNum, 9000);
         }
         else {
-            console.log('Error');// Can only happen if another program sets position
+            console.log('Error'); // Can only happen if another program sets position
         }
     }
 );
@@ -52,4 +52,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
--- software/examples/javascript/ExampleConfiguration.js
+++ software/examples/javascript/ExampleConfiguration.js
@@ -2,46 +2,47 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '6qCdfo';// Change to your UID
+var UID = '6qCdfo'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var servo = new Tinkerforge.BrickServo(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var servo = new Tinkerforge.BrickServo(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Configure two servos with voltage 5.5V
         // Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
         // and operating angle -100 to 100°
-        // Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95 
+        // Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95
         // to 1.95ms and operating angle -90 to 90°
         servo.setOutputVoltage(5500);
         servo.setDegree(0, -10000, 10000);
         servo.setPulseWidth(0, 1000, 2000);
         servo.setPeriod(0, 19500);
-        servo.setAcceleration(0, 1000);//Slow acceleration
-        servo.setVelocity(0, 0xFFFF);//Full speed
+        servo.setAcceleration(0, 1000); // Slow acceleration
+        servo.setVelocity(0, 0xFFFF); // Full speed
         servo.setDegree(5, -9000, 9000);
         servo.setPulseWidth(5, 950, 1950);
         servo.setPeriod(5, 20000);
-        servo.setAcceleration(5, 0xFFFF);//Full acceleration
-        servo.setVelocity(5, 0xFFFF);//Full speed
-        servo.setPosition(0, 10000);//Set to most right position
+        servo.setAcceleration(5, 0xFFFF); // Full acceleration
+        servo.setVelocity(5, 0xFFFF); // Full speed
+        servo.setPosition(0, 10000); // Set to most right position
         servo.enable(0);
-        servo.setPosition(5, -9000);//Set to most left position
-        servo.enable(5);      
+        servo.setPosition(5, -9000); // Set to most left position
+        servo.enable(5);
     }
 );
 
-ipcon.connect(HOST, PORT,
-    function(error) {
-        console.log('Error: '+error);        
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
     }
-);// Connect to brickd
-
+);
