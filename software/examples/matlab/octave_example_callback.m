c7a96da9d1013472e3ce5e4e3625dcbb605fccc4
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-03-25 12:04:47 +0100

    matlab: adding examples

---
+++ software/examples/matlab/octave_example_callback.m
@@ -0,0 +1,44 @@
+function octave_example_callback
+    more off;
+    
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "5VF5vG"; % Change to your UID
+
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    global servo;
+    servo = java_new("com.tinkerforge.BrickServo", UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Register "position reached callback" to cb_reached
+    % cb_reached will be called every time a position set with
+    % set_position is reached
+    servo.addPositionReachedListener("cb_reached");
+    servo.enablePositionReachedCallback();
+
+    % Set velocity to 100°/s. This has to be smaller or equal to 
+    % maximum velocity of the servo, otherwise cb_reached will be
+    % called too early
+    servo.setVelocity(0, 10000); 
+    servo.setPosition(0, 9000);
+    servo.enable(0);
+
+    input("\nPress any key to exit...\n", "s");
+    ipcon.disconnect();
+end
+
+% Use position reached callback to swing back and forth
+function cb_reached(servo_num, position)
+    global servo;
+    if str2double(position.toString()) == 9000
+        fprintf("Position: 90°, going to -90°\n");
+        servo.setPosition(servo_num, -9000);
+    elseif str2double(position.toString()) == -9000
+        fprintf("Position: -90°, going to 90°\n");
+        servo.setPosition(servo_num, 9000);
+    else
+        fprintf("Error\n"); % Can only happen if another program sets position
+    end
+end
---
+++ software/examples/matlab/octave_example_configuration.m
@@ -0,0 +1,42 @@
+function octave_example_configuration
+    more off;
+    
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "5VF5vG"; % Change to your UID
+
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    servo = java_new("com.tinkerforge.BrickServo", UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Configure two servos with voltage 5.5V
+    % Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
+    %          and operating angle -100 to 100°
+    %
+    % Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95 
+    %          to 1.95ms and operating angle -90 to 90°
+    servo.setOutputVoltage(5500);
+
+    servo.setDegree(0, -10000, 10000);
+    servo.setPulseWidth(0, 1000, 2000);
+    servo.setPeriod(0, 19500);
+    servo.setAcceleration(0, 1000); % Slow acceleration
+    servo.setVelocity(0, hex2num("FFFF")); % Full speed
+
+    servo.setDegree(5, -9000, 9000);
+    servo.setPulseWidth(5, 950, 1950);
+    servo.setPeriod(5, 20000);
+    servo.setAcceleration(5, hex2num("FFFF")); % Full acceleration
+    servo.setVelocity(5, hex2num("FFFF")); % Full speed
+
+    servo.setPosition(0, 10000); % Set to most right position
+    servo.enable(0);
+
+    servo.setPosition(5, -9000); % Set to most left position
+    servo.enable(5);
+
+    input("\nPress any key to exit...\n", "s");
+    ipcon.disconnect();
+end
