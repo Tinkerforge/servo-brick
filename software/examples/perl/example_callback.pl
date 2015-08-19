aa2919547d0b86732235bbf55466634bf15d3e53
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:34:36 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickServo;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '5VF5vG'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 our $servo = Tinkerforge::BrickServo->new(&UID, $ipcon); # Create device object
@@ -50,4 +50,3 @@ $servo->enable(0);
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_configuration.pl
+++ software/examples/perl/example_configuration.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickServo;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '5VF5vG'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $servo = Tinkerforge::BrickServo->new(&UID, $ipcon); # Create device object
@@ -17,7 +17,7 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Servo 1: Connected to port 0, period of 19.5ms, pulse width of 1 to 2ms
 #          and operating angle -100 to 100°
 #
-# Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95 
+# Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95
 #          to 1.95ms and operating angle -90 to 90°
 $servo->set_output_voltage(5500);
 
@@ -25,13 +25,13 @@ $servo->set_degree(0, -10000, 10000);
 $servo->set_pulse_width(0, 1000, 2000);
 $servo->set_period(0, 19500);
 $servo->set_acceleration(0, 1000); # Slow acceleration
-$servo->set_velocity(0, 0xFFFF); # Full speed
+$servo->set_velocity(0, 65535); # Full speed
 
 $servo->set_degree(5, -9000, 9000);
 $servo->set_pulse_width(5, 950, 1950);
 $servo->set_period(5, 20000);
-$servo->set_acceleration(5, 0xFFFF); # Full acceleration
-$servo->set_velocity(5, 0xFFFF); # Full speed
+$servo->set_acceleration(5, 65535); # Full acceleration
+$servo->set_velocity(5, 65535); # Full speed
 
 $servo->set_position(0, 10000); # Set to most right position
 $servo->enable(0);
@@ -41,5 +41,6 @@ $servo->enable(5);
 
 print "Press any key to exit...\n";
 <STDIN>;
+$servo->disable(0);
+$servo->disable(5);
 $ipcon->disconnect();
-
