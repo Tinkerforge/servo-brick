c42014a37e65047990bab74e8ee1eeb89f41819e
Matthias Bolte <matthias@tinkerforge.com>
2017-09-13 10:33:45 +0200

    java: Make PWM example make the C/C++ example match more closely

--- software/examples/java/ExamplePWMGenerator.java
+++ software/examples/java/ExamplePWMGenerator.java
@@ -1,5 +1,5 @@
-import com.tinkerforge.BrickServo;
 import com.tinkerforge.IPConnection;
+import com.tinkerforge.BrickServo;
 
 public class ExamplePWMGenerator {
     private static final String HOST = "localhost";
@@ -8,8 +8,14 @@ public class ExamplePWMGenerator {
     // Change XXYYZZ to the UID of your Servo Brick
     private static final String UID = "XXYYZZ";
 
+    // Due to the internal clock dividing mechanism of the Servo Brick not all
+    // arbitrary PWM frequency values can be achieved. For example, the upper
+    // most three available PWM frequency values are 1MHz, 500kHz and 250kHz.
+    // The steps are coarser on the high frequency end and much finer on the
+    // low end. You can set any value here between 15Hz and 1MHz and the Servo
+    // Brick will try to match it as closely as possible.
     private static int PWM_FREQUENCY = 175000; // in Hz [15Hz to 1MHz]
-    private static int PWM_DUTY_CYCLE = 100; // in % [0% to 100%]
+    private static int PWM_DUTY_CYCLE = 20; // in % [0% to 100%]
 
     private static short SERVO_NUMBER = 0; // [0 to 6]
 
@@ -23,15 +29,17 @@ public class ExamplePWMGenerator {
         // Don't use device before ipcon is connected
 
         // Set degree range to 0-100, this will allow to set the PWM duty cycle in 1% steps
-        servo.setDegree(SERVO_NUMBER, (short) 0, (short) 100);
+        servo.setDegree(SERVO_NUMBER, (short)0, (short)100);
 
         // Set PWM frequency (1-65535µs == 1MHz-15Hz)
         int period = 1000000 / PWM_FREQUENCY;
+
         if (period < 1) {
             period = 1; // 1MHz
         } else if (period > 65535) {
             period = 65535; // ~15Hz
         }
+
         servo.setPulseWidth(SERVO_NUMBER, 0, period);
         servo.setPeriod(SERVO_NUMBER, period);
 
@@ -41,18 +49,20 @@ public class ExamplePWMGenerator {
 
         // Set PWM duty cycle (0-100 %)
         int position = PWM_DUTY_CYCLE;
+
         if (position < 0) {
             position = 0;
         } else if (position > 100) {
             position = 100;
         }
-        servo.setPosition(SERVO_NUMBER, (short) position);
+
+        servo.setPosition(SERVO_NUMBER, (short)position);
 
         // Enable PWM signal
         servo.enable(SERVO_NUMBER);
 
         System.out.println("Press key to exit"); System.in.read();
-        servo.disable((short)0);
+        servo.disable(SERVO_NUMBER);
         ipcon.disconnect();
     }
 }
