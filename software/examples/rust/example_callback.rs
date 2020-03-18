e5e9f9e5f304982d9353c0a868cb93071ed485b5
Matthias Bolte <matthias@tinkerforge.com>
2020-03-18 17:40:13 +0100

    rust: Update to new skeleton

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,20 +1,21 @@
 use std::{error::Error, io, thread};
 use tinkerforge::{ip_connection::IpConnection, servo_brick::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Servo Brick
+const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Servo Brick.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let servo = ServoBrick::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let servo = ServoBrick::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     let position_reached_receiver = servo.get_position_reached_callback_receiver();
 
-    // Spawn thread to handle received callback messages. This thread ends when the servo
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `servo` object
     // is dropped, so there is no need for manual cleanup.
     let servo_copy = servo.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
