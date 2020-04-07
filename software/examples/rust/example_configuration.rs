2a2f724c1c26ea72779b5ea2468a4242eaaa7bd5
Erik Fleckstein <erik@tinkerforge.com>
2020-04-07 15:32:57 +0200

    examples: rust: Always block when calling bindings functions.

--- software/examples/rust/example_configuration.rs
+++ software/examples/rust/example_configuration.rs
@@ -19,32 +19,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     //
     // Servo 2: Connected to port 5, period of 20ms, pulse width of 0.95
     //          to 1.95ms and operating angle -90 to 90°
-    servo.set_output_voltage(5500);
+    servo.set_output_voltage(5500).recv()?;
 
-    servo.set_degree(0, -10000, 10000);
-    servo.set_pulse_width(0, 1000, 2000);
-    servo.set_period(0, 19500);
-    servo.set_acceleration(0, 1000); // Slow acceleration
-    servo.set_velocity(0, 65535); // Full speed
+    servo.set_degree(0, -10000, 10000).recv()?;
+    servo.set_pulse_width(0, 1000, 2000).recv()?;
+    servo.set_period(0, 19500).recv()?;
+    servo.set_acceleration(0, 1000).recv()?; // Slow acceleration
+    servo.set_velocity(0, 65535).recv()?; // Full speed
 
-    servo.set_degree(5, -9000, 9000);
-    servo.set_pulse_width(5, 950, 1950);
-    servo.set_period(5, 20000);
-    servo.set_acceleration(5, 65535); // Full acceleration
-    servo.set_velocity(5, 65535); // Full speed
+    servo.set_degree(5, -9000, 9000).recv()?;
+    servo.set_pulse_width(5, 950, 1950).recv()?;
+    servo.set_period(5, 20000).recv()?;
+    servo.set_acceleration(5, 65535).recv()?; // Full acceleration
+    servo.set_velocity(5, 65535).recv()?; // Full speed
 
-    servo.set_position(0, 10000); // Set to most right position
-    servo.enable(0);
+    servo.set_position(0, 10000).recv()?; // Set to most right position
+    servo.enable(0).recv()?;
 
-    servo.set_position(5, -9000); // Set to most left position
-    servo.enable(5);
+    servo.set_position(5, -9000).recv()?; // Set to most left position
+    servo.enable(5).recv()?;
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
 
-    servo.disable(0);
-    servo.disable(5);
+    servo.disable(0).recv()?;
+    servo.disable(5).recv()?;
 
     ipcon.disconnect();
     Ok(())
